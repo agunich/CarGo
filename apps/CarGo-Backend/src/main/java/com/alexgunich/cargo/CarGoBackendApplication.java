373b66ce44c193b3d59e4e031881b4de0b1f5c36
AGunich <138204726+cartierishere@users.noreply.github.com>
2024-12-16 23:42:20 +0300

    v1

---
+++ apps/CarGo-Backend/src/main/java/com/alexgunich/cargo/CarGoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.alexgunich.cargo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarGoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarGoBackendApplication.class, args);
+	}
+
+}
